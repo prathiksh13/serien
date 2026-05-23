aa672fdbe0d938d65e7e60b22d7439032b1fea56
prathiksh13 <prathiksh15@gmail.com>
2026-05-23 13:10:28 +0530

    Refactor Firebase config to use env variables
    Updated Firebase configuration to use environment variables for sensitive data.

--- frontend-react/src/lib/firebase.js
+++ frontend-react/src/lib/firebase.js
@@ -1,31 +1,36 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from 'firebase/firestore'
-import { getStorage } from 'firebase/storage'
-
-const storageBucket = import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "therasense-b0c8a.firebasestorage.app"
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "................",
-  authDomain: "................",
-  projectId: ".................",
-  storageBucket,
-  messagingSenderId: "...............",
-  appId: "..................",
-  measurementId: ".............."
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
 const firebaseAuth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
-const firestoreDb = getFirestore(app)
-const firebaseStorage = getStorage(app)
+const firestoreDb = getFirestore(app);
+const firebaseStorage = getStorage(app);
 
 let analytics = null;
+
 if (typeof window !== "undefined") {
   analytics = getAnalytics(app);
 }
 
-export { firebaseAuth, googleProvider, firestoreDb, firebaseStorage, analytics };
+export {
+  firebaseAuth,
+  googleProvider,
+  firestoreDb,
+  firebaseStorage,
+  analytics,
+};
